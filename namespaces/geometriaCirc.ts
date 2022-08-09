59c23586b0fc207cb05d29118ad297bf877e5ae4
Otavio Ramos <otavio.santana@pd.tec.br>
2022-08-09 14:48:08 -0300

    End of section about modules

---
+++ namespaces/geometriaCirc.ts
@@ -0,0 +1,9 @@
+namespace Geometria {
+    export namespace Areas {
+        const PI = 3.14
+        
+        export function areaCircunferencia(raio: number): number{
+            return PI * Math.pow(raio, 2);
+        } 
+    }
+}
---
+++ namespaces/namespaces.ts
@@ -0,0 +1,5 @@
+// const PI = 2.99;
+// console.log(PI);
+
+console.log(Geometria.Areas.areaCircunferencia(10));
+console.log(Geometria.Areas.areaRetangulo(10, 20));
