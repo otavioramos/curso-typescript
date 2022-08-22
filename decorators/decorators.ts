8e288862672ee258ed0a9a5e20537b254a093249
Otavio Ramos <otavio.santana@pd.tec.br>
2022-08-22 15:51:49 -0300

    End of lesson 136

--- decorators/decorators.ts
+++ decorators/decorators.ts
@@ -39,7 +39,7 @@ interface Eletrodomestico {
 // @decorator({ a: "teste", b: 123 })
 // @decorator({ a: "teste"})
 // @logarClasseSe(true)
-// @logarObjeto
+@logarObjeto
 @imprimivel
 class Eletrodomestico {
     constructor() {
@@ -57,3 +57,89 @@ function imprimivel(construtor: Function) {
 // (<any>new Eletrodomestico()).imprimir() // Cast para any - Não recomendável
 const eletro = new Eletrodomestico
 eletro.imprimir && eletro.imprimir()
+
+// Desafio Decorator perfilAdmin
+const usuarioLogado = {
+    nome: "Guilhemer Filho",
+    email: "guigui@gmail.com",
+    admin: true
+}
+
+@perfilAdmin
+class MudancaAdministrativa {
+    critico() {
+        console.log("Algo crítico foi alterado!");        
+    }
+}
+
+new MudancaAdministrativa().critico();
+
+function perfilAdmin<T extends Construtor>(construtor: T) {
+    return class extends construtor {
+        constructor(...args: any[]) {
+            super(...args);
+            if (!usuarioLogado || !usuarioLogado.admin) {
+                throw new Error("Forbidden error, you're not an admin");
+            }
+        }
+    }
+}
+
+class ContaCorrente {
+    @naoNegativo
+    private saldo: number;
+
+    constructor(saldo: number) {
+        this.saldo = saldo;
+    }
+
+    @congelar
+    sacar(valor: number) {
+        if (valor <= this.saldo) {
+            this.saldo -= valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @congelar
+    getSaldo() {
+        return this.saldo;
+    }
+}
+
+const cc = new ContaCorrente(10248.57);
+cc.sacar(5000);
+cc.sacar(5248.57);
+console.log(cc.getSaldo());
+
+// cc.getSaldo = function() {
+//     return this['saldo'] + 7000;
+// }
+// console.log(cc.getSaldo());
+
+// Object.freeze()
+function congelar(alvo: any, 
+        nomeMetodo: string, 
+        descriptor: PropertyDescriptor) {
+    console.log(alvo);
+    console.log(nomeMetodo);
+    descriptor.writable = false;
+}
+
+function naoNegativo(alvo: any, nomePropriedade: string) {
+    delete alvo[nomePropriedade];
+    Object.defineProperty(alvo, nomePropriedade, {
+        get: function(): any {
+            return alvo["_" + nomePropriedade];
+        },
+        set: function(valor: any): void {
+            if (valor < 0) {
+                throw new Error("Saldo inválido");
+            } else {
+                alvo["_" + nomePropriedade] = valor
+            }
+        }
+    });
+}
