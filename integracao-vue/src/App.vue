3482950dfc2578688f7c07afa784e80d0c05e679
Otavio Ramos <otavio.santana@pd.tec.br>
2022-08-31 11:26:05 -0300

    Creating component with a class

--- integracao-vue/src/App.vue
+++ integracao-vue/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <Contador :valorInicial="10" />
+    <ContadorClasse />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import Contador from "@/componentes/Contador.vue";
+import ContadorClasse from "@/componentes/ContadorClasse.vue";
 
 @Component({
   components: {
     Contador,
+    ContadorClasse
   },
 })
 export default class App extends Vue {}
